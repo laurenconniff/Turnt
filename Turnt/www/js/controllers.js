041ce210459edacc5943696504d90e3f269ec40e
Jesse Lurie <jesse@cuttlesoft.com>
2015-10-03 17:17:18 -0400

    web app

--- Turnt/www/js/controllers.js
+++ Turnt/www/js/controllers.js
@@ -25,4 +25,40 @@ angular.module('starter.controllers', [])
   $scope.settings = {
     enableFriends: true
   };
+})
+
+.controller('mapCtrl', function($cordovaGeolocation) {
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      var lat  = position.coords.latitude
+      var long = position.coords.longitude
+      console.log(lat);
+      console.log(long)
+    }, function(err) {
+      // error
+    });
+
+
+  var watchOptions = {
+    timeout : 3000,
+    enableHighAccuracy: false // may cause errors if true
+  };
+
+  var watch = $cordovaGeolocation.watchPosition(watchOptions);
+  watch.then(
+    null,
+    function(err) {
+      // error
+    },
+    function(position) {
+      var lat  = position.coords.latitude
+      var long = position.coords.longitude
+  });
+
+
+  watch.clearWatch();
+  
 });
---
+++ TurntWebApp/app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask, jsonify, url_for, render_template,request,redirect
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['POST','GET'])
+def index():
+	return render_template('index.html')
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
