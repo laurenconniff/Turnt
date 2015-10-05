51ab9ab7f07e0e9bac57afb256f8f12ff211544f
Jesse Lurie <jesse@cuttlesoft.com>
2015-10-04 20:00:35 -0400

    templates

--- TurntWebApp/app.py
+++ TurntWebApp/app.py
@@ -9,5 +9,23 @@ def index():
 	return render_template('index.html')
 
 
+@app.route('/home', methods=['POST','GET'])
+def home():
+	if request.method == "POST" and request.form['term'] != "NaN":
+		print request.form['dial']
+		return render_template('friends.html')
+
+	return render_template('home.html')
+
+
+@app.route('/friends',methods=['POST','GET'])
+def friends():
+	return render_template('friends.html')
+
+
+@app.route('/chats',methods=['POST','GET'])
+def chats():
+	return render_template('chats.html')
+
 if __name__ == "__main__":
 	app.run(debug=True)
